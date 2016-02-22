fd6ab2e55e7ea6f3909548fb9310c11c5d327a34
Luis Bravo <luisbravo258@gmail.com>
2016-02-22 10:14:09 -0400

    changes for web page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   root 'pages#home'
 
   get '/home', to: 'pages#home'
+  get '/about', to: 'pages#about'
+  get '/contact', to: 'pages#contact'
 
   resources :users
   resources :posts
