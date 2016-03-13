93126ae7389580ba5974f8f05c635a7750225190
Luis Bravo <luisbravo258@gmail.com>
2016-03-13 14:52:53 -0400

    changed pagination back to ten

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
-    @posts = Post.paginate(page: params[:page], per_page: 4)
+    @posts = Post.paginate(page: params[:page], per_page: 10)
   end
 
   def show
